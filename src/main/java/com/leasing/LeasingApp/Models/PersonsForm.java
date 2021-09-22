4d5fb136e41ae40a2825db2778099e6bcc574ad2
Rokenrolas <tuniekonematei@gmail.com>
2021-09-22 23:01:28 +0300

    Leasing application creation for main and co-applicant in progress

---
+++ src/main/java/com/leasing/LeasingApp/Models/PersonsForm.java
@@ -0,0 +1,15 @@
+package com.leasing.LeasingApp.Models;
+
+import java.util.ArrayList;
+
+public class PersonsArray {
+    private ArrayList<Person> personsList;
+
+    public ArrayList<Person> getPersonsList() {
+        return personsList;
+    }
+
+    public void setPersonsList(ArrayList<Person> personsList) {
+        this.personsList = personsList;
+    }
+}
